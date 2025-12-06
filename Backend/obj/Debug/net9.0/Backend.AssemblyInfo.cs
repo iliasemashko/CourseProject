cc2f720dde7ef34a0e83f1be0e257754c9edfde7
amadesya <ilik.2005@yandex.ru>
2025-12-07 00:23:35 +0300

    Теперь не выходит из аккаунта при перезагрузке. Почти

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29fd5f2576440c08a1e5c6402085c932f40348f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c5092c7857b6d71e84259cb59b7a8f8dc51baf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
